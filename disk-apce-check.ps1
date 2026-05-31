5ec36c479ff35cbdcbc4b2b730ed0f8c122d696b
petebyrne <91852327+petebyrne@users.noreply.github.com>
2026-05-31 14:22:33 +0100

    Update disk-apce-check.ps1

--- disk-apce-check.ps1
+++ disk-apce-check.ps1
@@ -1,1 +1,8 @@
+# disk-space-check.ps1
+# Checks local drive disk usage.
 
+Get-PSDrive -PSProvider FileSystem | Select-Object `
+    Name,
+    @{Name="UsedGB";Expression={[math]::Round($_.Used / 1GB, 2)}},
+    @{Name="FreeGB";Expression={[math]::Round($_.Free / 1GB, 2)}},
+    @{Name="TotalGB";Expression={[math]::Round(($_.Used + $_.Free) / 1GB, 2)}}
