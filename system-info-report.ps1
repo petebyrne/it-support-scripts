39318f38dd19ef05220e540b33f427195a42b834
petebyrne <91852327+petebyrne@users.noreply.github.com>
2026-05-31 14:24:52 +0100

    Update system-info-report.ps1

--- system-info-report.ps1
+++ system-info-report.ps1
@@ -1,1 +1,15 @@
+# system-info-report.ps1
+# Collects basic system information.
 
+$os = Get-CimInstance Win32_OperatingSystem
+$computer = Get-CimInstance Win32_ComputerSystem
+
+[PSCustomObject]@{
+    ComputerName = $env:COMPUTERNAME
+    LoggedInUser = $env:USERNAME
+    Manufacturer = $computer.Manufacturer
+    Model = $computer.Model
+    OperatingSystem = $os.Caption
+    OSVersion = $os.Version
+    LastBootTime = $os.LastBootUpTime
+}
