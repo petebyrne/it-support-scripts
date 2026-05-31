a995f7231e7fe20c7647f2f8972709fea10e5b53
petebyrne <91852327+petebyrne@users.noreply.github.com>
2026-05-31 14:28:51 +0100

    Update password-generator.ps1

--- password-generator.ps1
+++ password-generator.ps1
@@ -1,1 +1,9 @@
+# password-generator.ps1
+# Generates a random password for lab/testing use.
 
+$length = 16
+$characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!£$%^&*()-_=+'
+$password = -join ((1..$length) | ForEach-Object { $characters[(Get-Random -Maximum $characters.Length)] })
+
+Write-Host "Generated password:"
+Write-Host $password
